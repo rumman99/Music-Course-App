792bd853b2c915884f6497daf5c36e58f8c979fe
rumman99 <tasnimrumman369@gmail.com>
2024-04-12 19:17:11 +0600

    modify for deployment

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
             />
             <div className="p-4 relative z-10 w-full text-center">
                 <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400">Master the art of Music</h1>
-                <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into out comprehensive music courses and transform your musical journey today. Whether you're beginner or looking to refine your skills, joun us to unlock your true potential.</p>
+                <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Dive into out comprehensive music courses and transform your musical journey today. Whether youre beginner or looking to refine your skills, joun us to unlock your true potential.</p>
             </div>
             <div>
                 <Link href={'/courses'}>
